28868b4173121d5de326908d4b672be94aea8176
Bencso <gabor.bencsik13@gmail.com>
2017-01-11 12:02:10 +0100

    feat(web-exam): backend works, forntend renders responses, validator is not ready

--- client/script.js
+++ client/script.js
@@ -1,14 +1,5 @@
 'use strict';
 
-const exampleData = {
-  "status": "ok",
-  "projects": [
-    "secret project 1",
-    "secret project 2"
-  ]
-};
-
-
 const sendFeedback = (function () {
   const button = document.querySelector('button');
   const textarea = document.querySelector('textarea');
@@ -32,9 +23,10 @@ const changeUi = (function() {
   const section = document.querySelector('section');
   const span = document.querySelector('span');
 
-  let renderResponse = function (data) {
-    data.projects.forEach(function(item){
-      createListItem(item);
+  let renderResponse = function (projects) {
+    showContent();
+    projects.forEach(function(project){
+      createListItem(project);
     })
   };
 
@@ -54,40 +46,48 @@ const changeUi = (function() {
     span.classList.add('hide');
   };
 
+  let renderErrorMessage = function () {
+    span.classList.remove('hide');
+    span.innerHTML = 'Thank you!'
+  };
+
   return {
     renderResponse: renderResponse,
+    createListItem: createListItem,
     displayLoading: displayLoading,
-    showContent: showContent 
+    showContent: showContent,
+    renderErrorMessage: renderErrorMessage
   };
 
 }) ();
 
-
 const ajax = (function () {
 
   let postMessage = function (text, number, email) {
-    console.log(text + ' ' + number + ' ' + email);
+    let data = [];
     changeUi.displayLoading();
 
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
       if (xhr.readyState == XMLHttpRequest.DONE) {
-        JSON.parse(xhr.response);
+        console.log(JSON.parse(xhr.response));
+        data = JSON.parse(xhr.response);
+        if (data.status === "ok") {
+          changeUi.renderResponse(data.projects);
+        } else {
+          changeUi.renderErrorMessage(data.message);
+        };
       };
     };
 
     xhr.open('POST', 'http://localhost:3001/exam', true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send(JSON.stringify({"feedback": text, "scale": number, "email": email}));
-    console.log({"feedback": text, "scale": number, "email": email});
-
-    // changeUi.renderResponse(xhr.response.body);
-
-
   }
 
   return {
-    postMessage: postMessage
+    postMessage: postMessage,
   };
 
 }) ();
+console.log('script running');
--- server.js
+++ server.js
@@ -13,6 +13,8 @@ var connection = mysql.createConnection({
   database: 'secretprojects',
 });
 
+var niceWords = ["amazing", "awesome", "blithesome", "excellent", "fabulous", "fantastic", "favorable", "fortuitous", "great", "incredible", "ineffable", "mirthful", "outstanding", "perfect", "propitious", "remarkable", "smart", "spectacular", "splendid", "stellar", "stupendous", "super", "ultimate", "unbelievable", "wondrous"];
+
 connection.connect(function connectMsql(error) {
   if (error) {
     console.log('Connection failed', error);
@@ -32,8 +34,48 @@ app.use(function use(req, res, next) {
   next();
 });
 
+var validator = function (req) {
+  let text = req.body.feedback;         /* ezt még validálni kell */
+  let scale = parseInt(req.body.scale);
+  let email = req.body.email;
 
+  // function findNiceWords () {
+  //   niceWords.forEach
+  // };
 
+  if (email.indexOf != -1  && scale >= 10) {
+    return true;
+  } else {
+    return false;
+  };
+};
+
+module.exports = validator;
+
+app.post('/exam', function(req, res) {
+  console.log(req.body);
+  console.log(validator(req));
+  if (validator(req) == true) {
+    connection.query({
+      sql: 'SELECT project_name FROM projects'},
+      function sendBackProjects(err, rows) {
+        console.log(rows);
+      if (err) {
+        console.log(err.toString());
+        return;
+      }
+      let content = rows.map(function (e) {
+        return e.project_name;
+      });
+      console.log("yeah");
+      res.send({"status": "ok", "projects": content});
+      console.log({"status": "ok", "projects": content});
+    });
+  } else {
+    res.send({"status": "error", "message": "thank you"});
+  }
+
+});
 
 var port = process.env.PORT || 3001;
 app.listen(port, function () {
