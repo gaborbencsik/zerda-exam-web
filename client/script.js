1d6ef33550c9ff82372f7e2f21b809ac8ea36ede
Bencso <gabor.bencsik13@gmail.com>
2017-01-11 10:14:02 +0100

    feat(web-exam): post request ready

--- client/script.js
+++ client/script.js
@@ -1,1 +1,47 @@
 'use strict';
+
+const sendFeedback = (function () {
+  const button = document.querySelector('button');
+  const textarea = document.querySelector('textarea');
+  const number = document.querySelector('.number');
+  const email = document.querySelector('.email');
+
+  let send = function () {
+    ajax.postMessage(textarea.value, number.value, email.value);
+    // textarea.value = '';
+  };
+  button.addEventListener('click', send);
+
+  return {
+    send: send
+  }
+
+}) ();
+
+
+const ajax = (function () {
+
+  let postMessage = function (text, number, email) {
+    console.log(text + ' ' + number + ' ' + email);
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == XMLHttpRequest.DONE) {
+        JSON.parse(xhr.response);
+      };
+    };
+
+    xhr.open('POST', 'http://localhost:3000/exam', true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify({"feedback": text, "scale": number, "email": email}));
+    console.log({"feedback": text, "scale": number, "email": email});
+
+  }
+
+  return {
+    postMessage: postMessage
+  }
+
+
+}) ();
